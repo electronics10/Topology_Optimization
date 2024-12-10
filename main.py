a5e4011cfe15b16763b6ba4d56248828f4ea9aa8
electronics10 <73749204+electronics10@users.noreply.github.com>
2024-12-10 16:43:17 +0800

    exp18

---
+++ main.py
@@ -0,0 +1,36 @@
+import Antenna_Design as ad
+
+
+if __name__ == "__main__":
+    # Set excitation signal according to required specification
+    excitation_generator = ad.Excitation_Generator()
+    excitation_generator.amplitudes=[1]
+    excitation_generator.frequencies=[1.7]
+    excitation_generator.ratio_bw=[0.1]
+    excitation_generator.generate()
+    print("Spec_dictionary:", excitation_generator.spec_dic)
+    # excitation_generator.plot_wave_and_spectrum()
+
+    # Set initial antenna topology for the first iteration and start gradient ascent
+    # initial = ad.generate_shape("square")
+    initial = ad.generate_alphabet('A')
+    # initial = 0.5 * initial
+    print("Initial topology=\n", initial)
+    initial = initial.ravel()
+    
+    # Set optimizer
+    receiver = ad.Controller("CST_Antennas/receiver.cst")
+    transmitter = ad.Controller("CST_Antennas/transmitter.cst")
+    optimizer = ad.Optimizer(receiver, transmitter, set_environment=False)
+    optimizer.specification(excitation_generator.spec_dic, set_monitor=False)
+
+    # Topology optimization
+    alpha = 1
+    linear_map = True
+    filter = False
+    Adam = False
+    print(f"alpha={alpha}, linear_map={linear_map}, filter={filter}, Adam={Adam}")
+    optimizer.gradient_ascent(initial, alpha=alpha, iterations=60, linear_map=linear_map, filter=filter, Adam=Adam)
+
+    
+    
