2f7e032a25edc25e9ce8c3d0d4644b4c3acc51c5
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-03-07 11:53:16 +0800

    Update main.py 26

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     # Topology optimization
     alpha = 1
     linear_map = False
-    filter = False
-    Adam = False
+    filter = True
+    Adam = True
     print(f"alpha={alpha}, linear_map={linear_map}, filter={filter}, Adam={Adam}")
     optimizer.gradient_ascent(initial, alpha=alpha, iterations=60, linear_map=linear_map, filter=filter, Adam=Adam)
