592952aec6452994f7f2a09cbf8c94f595b61040
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-04-22 23:19:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     topop = ad.Controller("CST_Antennas/topop.cst")
     topop.delete_results()
     topop.set_time_solver()
-    optimizer = ad.Optimizer(topop, topop, set_environment=True)
+    optimizer = ad.Optimizer(topop, topop, set_environment=False)
     optimizer.specification(excitation_generator.spec_dic, set_monitor=True)
 
     ## Topology optimization
