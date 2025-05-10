2515d859187ddf15261c0876bd894839f0ca9f41
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-05-10 16:28:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,13 +25,13 @@ if __name__ == "__main__":
 
     ## Topology optimization
     # parameters
-    exp = "test3_3" # Legacy, not important
-    iter = 37
+    exp = "u_slot" # Legacy, not important
+    iter = 0
     alpha = 1
-    clean_legacy = False # set "False" for continuation, copy experiment results to global results folder
+    clean_legacy = True # set "False" for continuation, copy experiment results to global results folder
     linear_map = False
     filter = False
-    Adam = False
+    Adam = True
     print(f"alpha={alpha}, linear_map={linear_map}, filter={filter}, Adam={Adam}")
 
     # set initial antenna topology
