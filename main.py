9025ea9122488e4a4ef9504d9e25b5d74f59c9be
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-05-15 09:55:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 
     ## Topology optimization
     # parameters
-    exp = "single" # Legacy, not important
+    exp = "single1" # Legacy, not important
     iter = 0
     alpha = 1
     clean_legacy = True # set "False" for continuation, copy experiment results to global results folder
@@ -35,39 +35,43 @@ if __name__ == "__main__":
     print(f"alpha={alpha}, linear_map={linear_map}, filter={filter}, Adam={Adam}")
 
     # set initial antenna topology
+    def u_slot():
+            half = []
+            
+            H0 = 3
+            H1 = 2
+            # H3 = 8-H0-H1
+            L1 = 10
+            D1 = 3
+            L2 = 2
+            D2 = D1
+            
+            arr1 = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+            arr2 = []
+            arr3 = []
+            for j in range(16):
+                if j<D1: arr2.append(1)
+                elif j<D1+L1: arr2.append(0)
+                else: arr2.append(1)
+            for j in range(16):
+                if j<D2: arr3.append(1)
+                elif j<D2+L2: arr3.append(0)
+                else: arr3.append(1)
+            
+            for i in range(8):
+                if i < H0: half.append(arr1)
+                elif i < H0 + H1: half.append(arr2)
+                else: half.append(arr3)
+            
+            shape = half + half[::-1]
+            shape = np.array(shape)
+            return shape
+        
     if clean_legacy:
-        half = []
-        
-        H0 = 3
-        H1 = 2
-        # H3 = 8-H0-H1
-        L1 = 10
-        D1 = 3
-        L2 = 2
-        D2 = D1
-        
-        arr1 = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-        arr2 = []
-        arr3 = []
-        for j in range(16):
-            if j<D1: arr2.append(1)
-            elif j<D1+L1: arr2.append(0)
-            else: arr2.append(1)
-        for j in range(16):
-            if j<D2: arr3.append(1)
-            elif j<D2+L2: arr3.append(0)
-            else: arr3.append(1)
-        
-        for i in range(8):
-            if i < H0: half.append(arr1)
-            elif i < H0 + H1: half.append(arr2)
-            else: half.append(arr3)
-        
-        initial = half + half[::-1]
-        initial = np.array(initial)
-        # initial = ad.generate_shape("square")
+        # initial = u_slot()
+        initial = ad.generate_shape("square")
         # initial = ad.generate_shape("rectangle") 
-        # initial = initial*0.5
+        initial = initial*0.5
         initial = initial.ravel()
     else:
         initial, adam_var, power_init = ad.continue_iteration(exp, iter, alpha, Adam)
