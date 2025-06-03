4aaa80b1a3c4bfeac4705591f864cbfca0399898
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-06-03 21:56:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,9 +25,9 @@ if __name__ == "__main__":
 
     ## Topology optimization
     # parameters
-    exp = "hex6" # Legacy, not important
+    exp = "hex7" # Legacy, not important
     iter = 0
-    alpha = 1
+    alpha = 0.5
     clean_legacy = True # set "False" for continuation, copy experiment results to global results folder
     linear_map = False
     filter = False
