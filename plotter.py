ba0ae4fdc1b99e7ff1358cd7753e1e12e40d002a
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-06-26 01:25:52 +0800

    Update plotter.py

--- plotter.py
+++ plotter.py
@@ -1,4 +1,19 @@
-from Antenna_Design import Plotter
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+from math import ceil, sqrt
+from matplotlib import cm
+from matplotlib.colors import LinearSegmentedColormap
+
+# Get the original 'coolwarm' colormap
+coolwarm_cmap = cm.get_cmap('coolwarm')
+
+# Create a list of colors from the first half (0 to 0.5) of 'coolwarm'
+# We'll sample 256 colors for a smooth transition
+colors = [coolwarm_cmap(i) for i in np.linspace(0.5, 1, 256)]
+
+# Create a new colormap from this list of colors
+half_coolwarm_cmap = LinearSegmentedColormap.from_list("half_coolwarm", colors)
 
 
 if __name__ == "__main__":
