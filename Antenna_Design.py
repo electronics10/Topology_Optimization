9fe06bf595e99b36c9ef16607e0abe5d5379dd6d
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-06-26 01:26:50 +0800

    Update Antenna_Design.py

--- Antenna_Design.py
+++ Antenna_Design.py
@@ -16,24 +16,7 @@ from PIL import Image, ImageDraw, ImageFont
 import matplotlib.colors as colors
 from math import ceil, sqrt
 import difflib
-
-
-# Design parameter
-L = 36 # mm
-W = 36 # mm
-D = 2 # mm
-NX= int(L//D)
-NY = int(W//D)
-TSTEP = 0.1 # default 0.1 ns for 1~3 GHz
-TEND = 3.5 # default duration=3.5 ns for 1~3 GHz
-
-LG = 104
-WG = 104
-HC = 0.035
-HS = 1.6
-FEEDX = 5
-FEEDY = 0
-
+from settings import*
 
 class CSTInterface:
     def __init__(self, fname):
