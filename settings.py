d55e1de57ccb1758decbe83f588fbb32b6c29225
electronics10 <73749204+electronics10@users.noreply.github.com>
2025-06-26 01:27:16 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,15 @@
+# Design parameter
+L = 36 # mm
+W = 36 # mm
+D = 2 # mm
+NX= int(L//D)
+NY = int(W//D)
+TSTEP = 0.1 # default 0.1 ns for 1~3 GHz
+TEND = 3.5 # default duration=3.5 ns for 1~3 GHz
+
+LG = 104
+WG = 104
+HC = 0.035
+HS = 1.6
+FEEDX = 5
+FEEDY = 0
